901679772f4729702727a4fcf9a00fff8524b04e
Alejandro Sedano <alejandro@sistrategia.com>
2019-07-09 15:43:12 -0500

    Corrige fuente de rquirejs-text

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,13 @@ console.log("Require Js is working");
 require.config({
   paths: {
     'knockout': '../node_modules/knockout/build/output/knockout-latest.debug',
-    'jquery': '../node_modules/jquery/dist/jquery.min'
+    'jquery': '../node_modules/jquery/dist/jquery.min',
+    'req-txt':'../node_modules/requirejs-text/text'
   }
 });
 
-define(['jquery', 'knockout'], 
-  function($, ko) {
+define(['jquery', 'knockout','req-txt'], 
+  function($, ko,txt) {
     $(document).ready(function() {
      console.log("define working");
     });
